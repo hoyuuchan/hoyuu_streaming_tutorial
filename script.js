12abf81393ee8d283adaeccc3affc59f2013f652
hoyuuchan <ranhoyuu@gmail.com>
2025-01-11 00:14:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ const images = [
   { src: './image/동방/첸즌즌치키.png', tag: '~즌즌치키', category: '동방' },
   { src: './image/동방/유유코수박.png', tag: '~유유코수박', category: '동방' },
   { src: './image/동방/방방댄스.gif', tag: '~방방댄스', category: '동방' },
-  { src: './image/동방/니토리짝짝.gif', tag: '~니토리짝짝', category: '동방' },
+  { src: './image/동방/니토리짝짝.gif', tag: '~짝짝', category: '동방' },
   { src: './image/동방/소산이.png', tag: '~소산이', category: '동방' },
   { src: './image/동방/정엽이.png', tag: '~정엽이', category: '동방' },
   { src: './image/동방/미노리코.png', tag: '~미노리코', category: '동방' },
