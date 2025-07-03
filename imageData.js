274b846bdd576d1a229e9100b02dd8c6a97164cb
hoyuuchan <ranhoyuu@gmail.com>
2025-07-03 19:45:27 +0900

    Add files via upload

---
+++ imageData.js
@@ -0,0 +1,42 @@
+// imageData.js
+const allImagesData = [
+  {
+    path: 'https://github.com/hoyuuchan/hoyuu_streaming_tutorial/blob/main/image/3D/%EB%B8%94%EB%A0%8C%EB%8D%94.png',
+    keywords: ['블렌더'],
+    siteTag: '~블렌더', // 사이트에서 표시될 명령어
+    categories: ['3D', '블렌더']
+  },
+  {
+    path: './image/3D/블렌더응답없음.png',
+    keywords: ['블렌더응답없음'],
+    siteTag: '~블렌더응답없음',
+    categories: ['3D', '블렌더']
+  },
+  {
+    path: './image/3D/블렌더수잔.png',
+    keywords: ['블렌더수잔'],
+    siteTag: '~블렌더수잔',
+    categories: ['3D', '블렌더']
+  },
+  // 감정표현
+  {
+    path: './image/감정표현/마참내.png',
+    keywords: ['마참내'],
+    siteTag: '~마참내',
+    categories: ['감정표현']
+  },
+  {
+    path: './image/감정표현/두렵다.png',
+    keywords: ['두렵다'],
+    siteTag: '~두렵다',
+    categories: ['감정표현']
+  },
+  {
+    path: './image/감정표현/띠껍다.png',
+    keywords: ['띠껍다'],
+    siteTag: '~띠껍다',
+    categories: ['감정표현']
+  },
+  // ... (여기에 모든 이미지 데이터를 위와 같은 형식으로 추가해 주세요)
+  // 기존 dccon_list.js와 script.js에 있던 모든 이미지 데이터를 이 형식에 맞춰 넣어주시면 됩니다.
+];
