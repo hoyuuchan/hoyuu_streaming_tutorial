14a1a80f11bfa4c34c068358a436c52bd4240095
hoyuuchan <ranhoyuu@gmail.com>
2025-08-19 06:12:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -610,7 +610,7 @@ const images = [
   { src: './image/오리지널/d2.png', tag: '~오린따봉', category: '란호유' },
   { src: './image/오리지널/d3.png', tag: '~텐시엉엉', category: '란호유' },
   { src: './image/오리지널/d4.png', tag: '~레이무부적', category: '란호유' },
-  { src: './image/오리지널/d5.png', tag: '~쿄코진짜', category: '란호유' },
+  { src: './image/오리지널/d5.png', tag: '~ㅈㅉㅇㅇ', category: '란호유' },
   { src: './image/오리지널/d6.png', tag: '~다봤구나', category: '란호유' },
   { src: './image/오리지널/d7.png', tag: '~뀨', category: '란호유' },
   { src: './image/오리지널/d8.png', tag: '~구야꺼', category: '란호유' },
