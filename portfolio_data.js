28bdc43a585b5465e3ff38b8bcbecb2f5c710111
hoyuuchan <ranhoyuu@gmail.com>
2026-06-08 18:04:12 +0900

    링기링 코드 수정

--- portfolio_data.js
+++ portfolio_data.js
@@ -236,7 +236,7 @@ const portfolioData = [
   {
     "id": "ringgi_1780869484579",
     "year": "2026",
-    "title": "오토마치 우나",
+    "title": "오토마치 우나 Talk EX",
     "date": "2026.06.07",
     "mainImage": "./image/guide/ringgi/ì°ë.png",
     "mainImageStyle": "object-position: center;",
@@ -394,5 +394,5 @@ const portfolioData = [
 ];
 
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = portfolioData;
+  module.exports = portfolioData;
 }
